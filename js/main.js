9cf4f167d56874b84a5f2afa28f0a945b6489123
abaratov <abaratov@nurtelecom.kg>
2022-12-20 16:22:50 +0600

    added reviews block / partners block / catalog block

--- js/main.js
+++ js/main.js
@@ -40,3 +40,37 @@ var galleryThumbs = new Swiper('.gallery-thumbs', {
 galleryTop.controller.control = galleryThumbs;
 galleryThumbs.controller.control = galleryTop;
 
+// reviews
+
+var swiper = new Swiper(".revSwiper", {
+    slidesPerView: 2,
+    spaceBetween: 30,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+// partners
+
+var swiperPart = new Swiper(".partSwiper", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    freeMode: true,
+});
+
+// catalog
+
+var swiper = new Swiper(".catalogSwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 1,
+    speed: 900,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
